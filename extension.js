9bb4f424cb967733c24345ae041d0271e11e7f66
Sergio Costas <raster@rastersoft.com>
2019-08-09 10:09:38 +0200

    extension.js: Grouped all external variables
    Now uses a global dictionary to store all the external variables.
    This is more convenient.

--- extension.js
+++ extension.js
@@ -19,114 +19,138 @@
 
 const GLib = imports.gi.GLib;
 const Gio = imports.gi.Gio;
-
 const Shell = imports.gi.Shell;
+const Meta = imports.gi.Meta;
+
 const Layout = imports.ui.layout;
-const ExtensionUtils = imports.misc.extensionUtils;
 const Main = imports.ui.main;
+
+const ExtensionUtils = imports.misc.extensionUtils;
 const Mainloop = imports.mainloop;
 
-let appPid;
-let appUUID;
-let minx;
-let miny;
-let maxx;
-let maxy;
+//const appSys = Shell.AppSystem.get_default();
 
-let isEnabled;
-
-let idMap;
-let _monitorsChangedId;
-let _startupPreparedId;
-let _thumbnailScriptWatch;
-let _launchDesktopId;
-let _killAllInstancesId;
-let _currentProcess;
-let _windowUpdated = false;
-let _reloadTime;
-
-const appSys = Shell.AppSystem.get_default();
+let data = {};
 
 function init() {
-    isEnabled = false;
-    _thumbnailScriptWatch = 0;
-    _launchDesktopId = 0;
-    _killAllInstancesId = 0;
-    _currentProcess = null;
-    _reloadTime = 100;
+    data.isEnabled = false;
+    data.launchDesktopId = 0;
+    data.currentProcess = null;
+    data.reloadTime = 100;
+    data.metaDisplay = global.get_display();
     // Ensure that there aren't "rogue" processes
     doKillAllOldDesktopProcesses();
 }
 
 function enable() {
     if (Main.layoutManager._startingUp)
-        _startupPreparedId = Main.layoutManager.connect('startup-complete', () => innerEnable(true));
+        data.startupPreparedId = Main.layoutManager.connect('startup-complete', () => innerEnable());
     else
-        innerEnable(false);
+        innerEnable();
 }
 
-function innerEnable(disconnectSignal) {
-    if (disconnectSignal)
-        Main.layoutManager.disconnect(_startupPreparedId);
-
-    appUUID = GLib.uuid_string_random();
-    idMap = global.window_manager.connect('map', () => {
-        if (!_windowUpdated) {
-            for(let windowActor of global.get_window_actors()) {
-                let window = windowActor.get_meta_window();
-                let title = window.get_title();
-                if (title == appUUID) {
-                    window.move_resize_frame(false, minx, miny, maxx - minx, maxy - miny);
-                    window.stick();
-                    window.lower();
-                    _windowUpdated = true;
-                    // keep the window at the bottom
-                    window.connect_after('raised', () => {
-                        window.lower();
-                    });
-
-                }
+function getWindowActors() {
+    global.log("getWindowActors");
+    let retval = [];
+    let windowList = _oldGetWindowActors.bind(this)();
+    for (let windowActor of windowList) {
+        let window = windowActor.get_meta_window();
+        let title = window.get_title();
+        if (title == appUUID) {
+            _desktopWindow = window;
+            if (!windowActor._old_get_meta_window) {
+                windowActor._old_get_meta_window = windowActor.get_meta_window;
+                windowActor.get_meta_window = function() {
+                    global.log("Mi meta window");
+                    let window = this._old_get_meta_window.bind(this)();
+                    if (!window._old_get_property) {
+                        window._old_get_property = window.get_property;
+                        window.get_property = function(property_name, v2) {
+                            global.log("\n\n\n\nget property");
+                            global.log(property_name);
+                            global.log(v2);
+                            if (property_name == 'skip_taskbar') {
+                                let v = new GLib.Value();
+                                v.set_boolean(true);
+                                return v;
+                            } else {
+                                return window._old_get_property(property_name);
+                            }
+                        }
+                    }
+                    return window;
+                };
             }
         }
+        retval.push(windowActor);
+    }
+    return retval;
+}
+
+
+
+function innerEnable() {
+    if (data.startupPreparedId) {
+        Main.layoutManager.disconnect(data.startupPreparedId);
+        data.startupPreparedId = 0;
+    }
+
+    data.idMap = global.window_manager.connect('map', (obj, windowActor) => {
+        let window = windowActor.get_meta_window();
+        if (!data.windowUpdated) {
+            window.move_resize_frame(false, data.minx, data.miny, data.maxx - data.minx, data.maxy - data.miny);
+            window.stick();
+            window.lower();
+            data.windowUpdated = true;
+            // keep the window at the bottom
+            window.connect_after('raised', () => {
+                window.lower();
+            });
+        }
+        return false;
     });
 
     try {
-        _monitorsChangedId = Main.layoutManager.connect_after('monitors-changed', () => {
-            _reloadTime = 1000; // give more time in this case, to ensure that everything has changed
+        data.monitorsChangedId = Main.layoutManager.connect_after('monitors-changed', () => {
+            data.reloadTime = 1000; // give more time in this case, to ensure that everything has changed
             killCurrentProcess();
         });
     } catch(e) {
         // compatibility with 3.30
-        _monitorsChangedId = Main.layoutManager.connect('monitors-changed', () => {
-            _reloadTime = 2000; // give more time in this case, to ensure that everything has changed
+        data.monitorsChangedId = Main.layoutManager.connect('monitors-changed', () => {
+            data.reloadTime = 2000; // give more time in this case, to ensure that everything has changed
             killCurrentProcess();
         });
     }
-    isEnabled = true;
+    data.isEnabled = true;
     launchDesktop();
 }
 
 function disable() {
-    isEnabled = false;
+    data.isEnabled = false;
+    if (data.startupPreparedId)
+        Main.layoutManager.disconnect(data.startupPreparedId);
+    if (data.idMap)
+        global.window_manager.disconnect(data.idMap);
+    if (data.monitorsChangedId)
+        Main.layoutManager.disconnect(data.monitorsChangedId);
     killCurrentProcess();
-    global.window_manager.disconnect(idMap);
-    Main.layoutManager.disconnect(_monitorsChangedId);
 }
 
 function killCurrentProcess() {
-    if (_launchDesktopId) {
-        GLib.source_remove(_launchDesktopId);
-        _launchDesktopId = 0;
-        if (isEnabled) {
-            _launchDesktopId = Mainloop.timeout_add(_reloadTime, () => {
-                _launchDesktopId = 0;
+    if (data.launchDesktopId) {
+        GLib.source_remove(data.launchDesktopId);
+        data.launchDesktopId = 0;
+        if (data.isEnabled) {
+            data.launchDesktopId = Mainloop.timeout_add(data.reloadTime, () => {
+                data.launchDesktopId = 0;
                 launchDesktop();
             });
         }
     }
 
-    if (_currentProcess) {
-        _currentProcess.force_exit();
+    if (data.currentProcess) {
+        data.currentProcess.force_exit();
     }
 }
 
@@ -172,7 +196,7 @@ function doKillAllOldDesktopProcesses() {
 
 function launchDesktop() {
 
-    _reloadTime = 100;
+    data.reloadTime = 100;
     let argv = [];
     argv.push(GLib.build_filenamev([ExtensionUtils.getCurrentExtension().path, 'adieu.js']));
     argv.push("-P");
@@ -185,26 +209,27 @@ function launchDesktop() {
         let area = ws.get_work_area_for_monitor(monitorIndex);
         argv.push("-D");
         argv.push(area.x + ";" + area.y+";" + area.width + ";" + area.height);
-        if (first || (area.x < minx)) {
-            minx = area.x;
+        if (first || (area.x < data.minx)) {
+            data.minx = area.x;
         }
-        if (first || (area.y < miny)) {
-            miny = area.y;
+        if (first || (area.y < data.miny)) {
+            data.miny = area.y;
         }
-        if (first || ((area.x + area.width) > maxx)) {
-            maxx = area.x + area.width;
+        if (first || ((area.x + area.width) > data.maxx)) {
+            data.maxx = area.x + area.width;
         }
-        if (first || ((area.y + area.height) > maxy)) {
-            maxy = area.y + area.height;
+        if (first || ((area.y + area.height) > data.maxy)) {
+            data.maxy = area.y + area.height;
         }
         first = false;
     }
 
-    _windowUpdated = false;
+    data.windowUpdated = false;
+    data.appUUID = GLib.uuid_string_random();
     let launcher = new Gio.SubprocessLauncher({flags: Gio.SubprocessFlags.STDIN_PIPE | Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_MERGE});
     launcher.set_cwd(ExtensionUtils.getCurrentExtension().path);
-    _currentProcess = launcher.spawnv(argv);
-    _currentProcess.communicate_async(GLib.Bytes.new(appUUID + "\n"), null, (object, res) => {
+    data.currentProcess = launcher.spawnv(argv);
+    data.currentProcess.communicate_async(GLib.Bytes.new(data.appUUID + "\n"), null, (object, res) => {
         try {
             let [available, stdout, stderr] = object.communicate_finish(res);
             if (stdout.length != 0) {
@@ -214,23 +239,22 @@ function launchDesktop() {
             global.log("Error " + e);
         }
     });
-    global.log(appUUID);
-    appPid = Number(_currentProcess.get_identifier());
-    _currentProcess.wait_async(null, () => {
-        if (_currentProcess.get_if_exited()) {
-            let retval = _currentProcess.get_exit_status();
+    global.log(data.appUUID);
+    //appPid = Number(_currentProcess.get_identifier());
+    data.currentProcess.wait_async(null, () => {
+        if (data.currentProcess.get_if_exited()) {
+            let retval = data.currentProcess.get_exit_status();
             if (retval != 0) {
-                _reloadTime = 1000;
+                data.reloadTime = 1000;
             }
         }
-        _currentProcess = null;
-        appPid = 0;
-        if (isEnabled) {
-            if (_launchDesktopId) {
-                GLib.source_remove(_launchDesktopId);
+        data.currentProcess = null;
+        if (data.isEnabled) {
+            if (data.launchDesktopId) {
+                GLib.source_remove(data.launchDesktopId);
             }
-            _launchDesktopId = Mainloop.timeout_add(_reloadTime, () => {
-                _launchDesktopId = 0;
+            data.launchDesktopId = Mainloop.timeout_add(data.reloadTime, () => {
+                data.launchDesktopId = 0;
                 launchDesktop();
             });
         }
