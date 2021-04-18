616056dc6c7897a2f877eeb7fcd1237cc4c077a1
Sergio Costas <rastersoft@gmail.com>
2021-04-18 10:41:19 +0000

    Revert "Merge branch 'reduce-rubber-band-lag' into 'master'"
    This reverts merge request !88

--- desktopGrid.js
+++ desktopGrid.js
@@ -138,8 +138,7 @@ var DesktopGrid = class {
             return false;
         });
         this._eventBox.connect('motion-notify-event', (actor, event) => {
-            let device = event.get_device();
-            let [a, x, y] = device ? device.get_position() : event.get_coords();
+            let [a, x, y] = event.get_coords();
             [x, y] = this._coordinatesLocalToGlobal(x, y);
             this._desktopManager.onMotion(x, y);
         });
